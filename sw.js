3560b0e560d2235adb049681df5a898afadec498
David-Jhon <106485025+David-Jhon@users.noreply.github.com>
2025-11-14 09:08:33 +0000

    sw.js

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
 
-  if (event.request.method === 'POST' && url.pathname === '/share-target') {
+  if (event.request.method === 'POST' && (url.pathname === '/' || url.pathname === '/share-target')) {
     event.respondWith(Response.redirect('/'));
     event.waitUntil(async function () {
       const formData = await event.request.formData();
